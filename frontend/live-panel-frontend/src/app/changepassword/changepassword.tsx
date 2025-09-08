5219db2e5500bf2d9c46762ebe34f0e50d5bb8ce
thisisveryfunny <returnnothing0@gmail.com>
2025-09-08 13:23:58 -0400

    test update

--- frontend/live-panel-frontend/src/app/changepassword/changepassword.tsx
+++ frontend/live-panel-frontend/src/app/changepassword/changepassword.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React, {useState} from 'react'
-import Buttons from "$/Components/Buttons/Buttons"
+import Buttons from "$/Components/Buttons/Buttons";
 
 export default function ChangePassword(){
 
