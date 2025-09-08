305b2cf27c1cd0f5f2cb05638b36ae1a148bbed6
nicolas arsenault <nicolasarsenault@nicolass-MacBook-Pro.local>
2025-09-08 12:41:11 -0400

    Added login, register and changepassword

--- backend/src/main/java/com/phishplanet/backend/controllers/AuthController.java
+++ backend/src/main/java/com/phishplanet/backend/controllers/AuthController.java
@@ -2,10 +2,18 @@ package com.phishplanet.backend.controllers;
 
 import com.phishplanet.backend.dto.AuthRequest;
 import com.phishplanet.backend.dto.AuthResponse;
+import com.phishplanet.backend.dto.ChangePasswordRequest;
+import com.phishplanet.backend.dto.RegisterRequest;
+import com.phishplanet.backend.entities.User;
+import com.phishplanet.backend.repository.UserRepository;
 import com.phishplanet.backend.utils.JwtProvider;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,13 +23,55 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/auth")
 public class AuthController {
 
-    private AuthenticationManager authenticationManager;
-    private JwtProvider jwtProvider;
+    private final AuthenticationManager authenticationManager;
+    private final JwtProvider jwtProvider;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
 
-    public AuthController(AuthenticationManager authenticationManager,JwtProvider jwtProvider)
+    public AuthController(AuthenticationManager authenticationManager,JwtProvider jwtProvider, UserRepository userRepository,
+                          PasswordEncoder passwordEncoder)
     {
         this.authenticationManager = authenticationManager;
         this.jwtProvider = jwtProvider;
+        this.passwordEncoder = passwordEncoder;
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping("/register")
+    public String register (@RequestBody RegisterRequest req)
+    {
+        if(userRepository.findByUsername(req.getUsername()).isPresent()){
+            return "Username already exists";
+        }
+
+        if(req.getPassword() != req.getPassword_confirm())
+        {
+            return "Passwords dont match";
+        }
+
+        User newUser = new User();
+        newUser.setUsername(req.getUsername());
+        newUser.setPassword(passwordEncoder.encode(req.getPassword()));
+        newUser.setRole("ROLE_USER");
+
+        userRepository.save(newUser);
+        return "Registration successfull!";
+    }
+
+    @PostMapping("/changepassword")
+    public String changePassword(@RequestBody ChangePasswordRequest req)
+    {
+
+        User user = userRepository.findByUsername(req.getUsername()).orElseThrow(()->new RuntimeException("User not found"));
+
+        if(!passwordEncoder.matches(req.getOldPassword(), user.getPassword())){
+            return "Old password is incorrect.";
+        }
+
+        user.setPassword(passwordEncoder.encode(req.getNewPassword()));
+        userRepository.save(user);
+
+        return "Password changed successfully";
     }
 
     @PostMapping("/login")
@@ -39,4 +89,6 @@ public class AuthController {
             throw new RuntimeException("Invalid username or password!");
         }
     }
+
+
 }
