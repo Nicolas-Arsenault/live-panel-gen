1dd829c81813ba0533c792ce23b3f5d7e3592def
nicolas arsenault <nicolasarsenault@nicolass-MacBook-Pro.local>
2025-09-08 14:10:21 -0400

    changed clients to users and its fields to match updates

--- postgres-docker/init.sql
+++ postgres-docker/init.sql
@@ -1,8 +1,10 @@
-CREATE TABLE Client (
-    clientId UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    stats INTEGER DEFAULT 0,
-    nb_live_user INTEGER DEFAULT 0
+CREATE TABLE users (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(255)
 );
+
 
 CREATE TABLE VPS (
     id SERIAL PRIMARY KEY,
