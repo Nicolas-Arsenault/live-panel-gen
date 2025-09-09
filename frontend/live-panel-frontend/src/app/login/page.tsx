d47ad2674d6e93208fdffa3b513c325d006b4da3
thisisveryfunny <returnnothing0@gmail.com>
2025-09-09 17:04:31 -0400

    login logic frontend

--- frontend/live-panel-frontend/src/app/login/page.tsx
+++ frontend/live-panel-frontend/src/app/login/page.tsx
@@ -3,18 +3,35 @@
 import React, {useState} from 'react'
 import Buttons from "$/Components/Buttons/Buttons"
 import Link from "next/link"
+import { SubmitHandler, useForm } from "react-hook-form"
+import { z } from "zod"
+import { zodResolver  } from "@hookform/resolvers/zod"
+
+const schema = z.object({
+    username: z.string(),
+    password: z.string(),
+}).required();
+
+type FormFields = z.infer<typeof schema>;
 
 export default function Login(){
+    const {
+        register,
+        handleSubmit,
+        setError,
+        formState: { errors },
+    } = useForm<FormFields>({
+        resolver: zodResolver(schema),
+    });
 
-    const [login, setLogin] = useState();
+    const onSubmit = (data: FormFields) => {
+        try{
+            
+        }
+        catch{
 
-    let userName;
-    let passWord;
-
-    function handleLogin(){
-        setLogin(login);
+        }
     }
-
     return(
         <div className="font-sans bg-black text-[rgb(126,126,126)] min-h-screen m-0 w-full h-full absolute bg-gradient-to-br from-neutral-600 to-black">
             <div className="background-animation">
@@ -25,11 +42,16 @@ export default function Login(){
                             <h1 className="text-white text-[1.75em] text-shadow-white-5g">PhishHub</h1>
                         </div>
                         <div className="flex flex-col gap-2">
-                            <form onSubmit={handleLogin}>
+                            <form onSubmit={handleSubmit(onSubmit)}>
+
                                 <label className="text-[rgb(106,106,106)]">Username</label>
-                                <input className="border border-[#202225] bg-[#111214] p-3 text-white font-sans rounded-md focus:outline-none focus:border-[#4dff00]" value={userName} placeholder="Your Username" type="text" />
+                                <input {...register("username")} className="border border-[#202225] bg-[#111214] p-3 text-white font-sans rounded-md focus:outline-none focus:border-[#4dff00]" placeholder="Your Username" type="text" />
+                                {errors.username && <p>{errors.username.message}</p>}
+                                
                                 <label className="text-[rgb(106,106,106)]">Password</label>
-                                <input className="border border-[#202225] bg-[#111214] p-3 text-white font-sans rounded-md focus:outline-none focus:border-[#4dff00]" value={passWord} placeholder="Your Password" type="password" />
+                                <input {...register("password")} className="border border-[#202225] bg-[#111214] p-3 text-white font-sans rounded-md focus:outline-none focus:border-[#4dff00]" placeholder="Your Password" type="password" />
+                                {errors.password && <p>{errors.password.message}</p>}
+
                                 <Buttons text="Login"/>
                             </form>
                             <Link href={"/changepassword"} className={"text-[#4d4d4d] no-underline transition-colors duration-500 ease-in-out hover:text-white cursor-pointer"}>Change Your Password</Link>
