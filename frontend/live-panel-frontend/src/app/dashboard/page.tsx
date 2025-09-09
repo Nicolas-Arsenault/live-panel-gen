9d45bde99fe80b580a2bc57f7432c8482dbfd4ad
thisisveryfunny <returnnothing0@gmail.com>
2025-09-08 22:09:57 -0400

    edited dashboard home

--- frontend/live-panel-frontend/src/app/dashboard/page.tsx
+++ frontend/live-panel-frontend/src/app/dashboard/page.tsx
@@ -6,8 +6,22 @@ export default function Dashboard(){
         //     window.location.replace("/login")
         // </script>
         <>
-            
+        <div>
             <SideMenu/>
+        </div>
+            <div>
+                <div className="grid-cols-3 gap-15 h-top mt-20 flex items-center justify-center">
+                    <div className="p-25 border-4 border-[#1c1d20] bg-[#111214] rounded-lg">
+                        <label>Number of Results:</label>
+                    </div>
+                    <div className="p-25 border-4 border-[#1c1d20] bg-[#111214] rounded-lg">
+                        <label>Current Live Visitors:</label>
+                    </div>
+                    <div className="p-25 border-4 border-[#1c1d20] bg-[#111214] rounded-lg">
+                        <label>Active pages:</label>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
